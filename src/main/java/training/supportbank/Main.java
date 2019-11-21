4b85627daf1e8e12d0c4ff8f4839411592917cf2
Olaf Davis <olaf.davis@softwire.com>
2019-11-21 13:56:25 +0000

    starting

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,8 +1,29 @@
 package training.supportbank;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String args[]) {
         // Your code here!
-        System.out.println("Test!");
+        System.out.println("Test!!");
+        Stream<String> lines = readFile();
     }
+
+    static Stream<String> readFile()  {
+        Stream<String> lines = Stream.empty();
+        Path path = Paths.get("Transactions2014.csv");
+        try {
+            lines = Files.lines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+
+
 }
---
+++ src/main/java/training/supportbank/User.java
@@ -0,0 +1,7 @@
+package training.supportbank;
+
+class User {
+    private String name;
+    private Integer balancePence;
+    public User(String name)
+}
